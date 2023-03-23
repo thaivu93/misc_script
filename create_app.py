3261c8625c5860e01b9b2b8843b01e2d82494dfb
Victor Ng <thaivu.tcnh2@gmail.com>
2023-03-23 12:50:26 +1100

    Add files via upload

--- create_app.py
+++ create_app.py
@@ -1,17 +1,16 @@
 import os
 
-# class definition
-
+# Class Definition
 class CreateApp():
 
     def __init__(self, app_name, app_type) -> None:
         self.app_name = app_name
         self.app_type = app_type
         self.app_loc = os.getcwd()
+        self.path = os.path.join(self.app_loc, self.app_name)
 
     # Check if the application is already existed
     def check_existence(self):
-        self.path = os.path.join(self.app_loc, self.app_name)
         return os.path.exists(self.path)
 
     def create_files(self, files):
@@ -33,39 +32,33 @@ class CreateApp():
 
     def app_init(self):
         types = ['front-end', 'flask']
-        os.mkdir(os.path.join(self.path))
-        if self.app_type == types[0]:
-            files = ['index.html', 'styles.css', 'script.js']
-            self.create_files(files)
-        elif self.app_type == types[1]:
-            folders = ['static', 'templates', 'images']
-            files = ['app.py', 'config.py', 'models.py']
-            self.create_folders(folders)
-            self.create_files(files)
+        if self.check_existence() == False:
+            os.mkdir(self.path)
+            if self.app_type == types[0]:
+                files = ['index.html', 'styles.css', 'script.js']
+                self.create_files(files)
+            elif self.app_type == types[1]:
+                folders = ['static', 'templates', 'images']
+                files = ['app.py', 'config.py', 'models.py']
+                self.create_folders(folders)
+                self.create_files(files)
+            else:
+                pass
         else:
             pass
-
     def __repr__(self):
         print(f'APP NAME:{self.app_name}, APP_TYPE: {self.app_type}')
 
-# test = CreateApp('test','flask')
-# test.check_existence()
-# test.app_init()
-
-# test1 = CreateApp('test1', 'front-end')
-# test1.check_existence()
-# test1.app_init()
-
-
+# Program data and loop
+types = ['front-end', 'flask']
+is_on = True
 # Program logic
-while True:
-    status = True
+while is_on:
     print('Generating project using this script.')
     option = input('Do you want to create new project? y/n \n').lower()
     if option == 'n':
-        status = False
+        is_on = False
     elif option == 'y':
-        types = ['front-end', 'flask']
         app_name = input('Enter application name: ')
         print('Select your app types: ')
         print('1. Front End')
@@ -73,21 +66,15 @@ while True:
         select = int(input('1 or 2: '))
         if select == 1:
             app = CreateApp(app_name, types[0])
-            if app.check_existence() == False:
-                app.app_init()
-                status = False
-            else:
-                pass
+            app.app_init()
+            is_on = False
         elif select == 2:
             app = CreateApp(app_name, types[1])
-            if app.check_existence() == False:
-                app.app_init()
-                status = False
-            else:
-                pass
+            app.app_init()
+            is_on = False
         else:
             pass
     else:
         print('Invalid options. Choose again:')
-    if status == False:
+    if is_on == False:
         break
